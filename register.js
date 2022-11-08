3c0e9bf7469e754b2ad081940f24694252f573e9
Xellter <95046520+Xellter@users.noreply.github.com>
2022-11-08 17:35:41 +0100

    Added authenitication

---
+++ register.js
@@ -0,0 +1,22 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+export const initRegisterForm = (auth) => {
+	const registerForm = document.querySelector("#registerForm");
+	if (registerForm) {
+		registerForm.addEventListener("submit", (event) => {
+			event.preventDefault();
+			const formData = new FormData(registerForm);
+			createUserWithEmailAndPassword(
+				auth,
+				formData.get("email"),
+				formData.get("password")
+			)
+				.then((result) => {
+					window.location.href = window.location.origin;
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		});
+	}
+};
